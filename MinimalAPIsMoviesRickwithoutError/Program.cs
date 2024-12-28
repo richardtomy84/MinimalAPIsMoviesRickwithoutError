02573c228d55bee2ee4fe786bdc00dec24b3dd12
Richard Tomy <richardtomy84@outlook.com>
2024-12-28 17:32:16 +0530

    Nullable Example

--- MinimalAPIsMoviesRickwithoutError/Program.cs
+++ MinimalAPIsMoviesRickwithoutError/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//string lastName = "Gavilan";
+string? lastName = null;
+
+string lastNameInUpperCase = lastName?.ToUpper();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World! " + lastNameInUpperCase);
 
 app.Run();
