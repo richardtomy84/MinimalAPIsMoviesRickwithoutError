0c04a3656154207ff33b5156684c360647a15f49
Richard Tomy <richardtomy84@outlook.com>
2025-01-05 16:35:58 +0530

    Program.cs class edited

--- MinimalAPIsMoviesRickwithoutError/Program.cs
+++ MinimalAPIsMoviesRickwithoutError/Program.cs
@@ -1,16 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
 
 //string lastName = "Gavilan";
+//Services Zone - Begin
+
+
+
 string? lastName = null;
 
 var lastNameProject = builder.Configuration.GetValue<string>("lastName");
 
 string lastNameInUpperCase = lastName?.ToUpper();
 
-
+//Services Zone - End
 
 var app = builder.Build();
 
+
+//Middlewares Zone- Begin
+
 app.MapGet("/", () => "Hello World! " + lastNameInUpperCase + "\n"+ lastNameProject);
+
+//Middlewares Zone- End
 
 app.Run();
