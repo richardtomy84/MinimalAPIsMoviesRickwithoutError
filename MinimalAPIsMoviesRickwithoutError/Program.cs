a7371d76b75efd2a6f98befbd5232b0b9a4fb968
Richard Tomy <richardtomy84@outlook.com>
2025-01-05 16:29:01 +0530

    // "ASPNETCORE_ENVIRONMENT": "Development",        "ASPNETCORE_ENVIRONMENT": "Production"  changes in appsettings and Launchsettings

--- MinimalAPIsMoviesRickwithoutError/Program.cs
+++ MinimalAPIsMoviesRickwithoutError/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 //string lastName = "Gavilan";
 string? lastName = null;
 
+var lastNameProject = builder.Configuration.GetValue<string>("lastName");
 
 string lastNameInUpperCase = lastName?.ToUpper();
 
@@ -10,6 +11,6 @@ string lastNameInUpperCase = lastName?.ToUpper();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World! " + lastNameInUpperCase);
+app.MapGet("/", () => "Hello World! " + lastNameInUpperCase + "\n"+ lastNameProject);
 
 app.Run();
